7447b41a741f35066dfb3e8c7896b0f20db17cb8
oslinjohn20 <ossiekcjohnson@gmail.com>
2022-04-21 15:22:30 +0200

    calender_completed_with_firestore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import {initializeApp} from "firebase/app";
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore'
 import VueTextareaAutosize from 'vue-textarea-autosize';
 
 
@@ -9,7 +10,7 @@ Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false;
 
-const firebaseConfig= ({
+firebase.initializeApp({
   apiKey: "AIzaSyDT47-eSfdsbjw3uO68vHSoBVlDAYMj1oM",
   authDomain: "vue-calendar-6f06f.firebaseapp.com",
   projectId: "vue-calendar-6f06f",
@@ -18,7 +19,7 @@ const firebaseConfig= ({
   appId: "1:15994349727:web:f5dafeb8b378b25a241aaa"
 });
 
-export const db = initializeApp(firebaseConfig);
+export const db = firebase.firestore();
 
 
 new Vue({
